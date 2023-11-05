cf23e8b23161a77a81bbdc21ab81b41d364f48ef
xcaboclean <146889041+xcaboclean@users.noreply.github.com>
2023-11-05 02:48:12 +0000

    new feat function format_name

---
+++ main.py
@@ -0,0 +1,6 @@
+def format_name(f_name, l_name):
+  formated_f_name = f_name.title()
+  formated_l_name = l_name.title()
+  print(f"{formated_f_name} {formated_l_name}")
+
+format_name("AnDRI", "SOUZA")
