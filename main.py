d3a0e2e474664be5f6d34330345c513d8e4131a6
xcaboclean <146889041+xcaboclean@users.noreply.github.com>
2023-11-06 01:31:08 +0000

    docstring

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
+'''Take a first and last name and format ir to return the title case version of the name.'''
 def format_name(f_name, l_name):
+  if f_name == "" or l_name == "":
+    return "You didn't provide valid inputs."
   formated_f_name = f_name.title()
   formated_l_name = l_name.title()
   return f"{formated_f_name} {formated_l_name}"
